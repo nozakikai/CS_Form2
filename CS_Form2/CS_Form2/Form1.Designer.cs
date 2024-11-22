f9ef82cd7fd685e29b408306a7c0138f92ded062
MSI\kai08 <zeal24360040@fsgcl.jp>
2024-11-22 16:08:17 +0900

    画像の表示

--- CS_Form2/CS_Form2/Form1.Designer.cs
+++ CS_Form2/CS_Form2/Form1.Designer.cs
@@ -30,41 +30,43 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.labelCPU = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelPLAYER = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(78, 85);
+            this.panel1.Location = new System.Drawing.Point(31, 255);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.Size = new System.Drawing.Size(250, 250);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // labelCPU
             // 
             this.labelCPU.AutoSize = true;
-            this.labelCPU.Location = new System.Drawing.Point(729, 37);
+            this.labelCPU.Location = new System.Drawing.Point(439, 9);
             this.labelCPU.Name = "labelCPU";
             this.labelCPU.Size = new System.Drawing.Size(121, 18);
             this.labelCPU.TabIndex = 1;
             this.labelCPU.Text = "CPUの出した手";
             // 
-            // label1
+            // labelPLAYER
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "自分の出した手";
+            this.labelPLAYER.AutoSize = true;
+            this.labelPLAYER.Location = new System.Drawing.Point(45, 205);
+            this.labelPLAYER.Name = "labelPLAYER";
+            this.labelPLAYER.Size = new System.Drawing.Size(122, 18);
+            this.labelPLAYER.TabIndex = 2;
+            this.labelPLAYER.Text = "自分の出した手";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(52, 384);
+            this.button1.Location = new System.Drawing.Point(99, 556);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 36);
             this.button1.TabIndex = 3;
@@ -74,7 +76,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(166, 384);
+            this.button2.Location = new System.Drawing.Point(480, 556);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(108, 36);
             this.button2.TabIndex = 4;
@@ -83,23 +85,41 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(280, 384);
+            this.button3.Location = new System.Drawing.Point(872, 556);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(108, 36);
             this.button3.TabIndex = 5;
             this.button3.Text = "パー";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(404, 255);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 250);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(812, 255);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(250, 250);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 614);
+            this.ClientSize = new System.Drawing.Size(1387, 697);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPLAYER);
             this.Controls.Add(this.labelCPU);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -112,10 +132,12 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label labelCPU;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPLAYER;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- CS_Form2/CS_Form2/Form1.cs
+++ CS_Form2/CS_Form2/Form1.cs
@@ -12,10 +12,12 @@ namespace CS_Form2
 {
     public partial class Form1 : Form
     {
-      
+
 
         //画像ファイルを読み込む
-        Image currentImage = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
+        Image currentImage1 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
+        Image currentImage2 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\チョキ.png");
+        Image currentImage3 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\パー.png");
         public Form1()
         {
             InitializeComponent();
@@ -26,25 +28,48 @@ namespace CS_Form2
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            MessageBox.Show("sakuma ゆいと");
         }
 
 
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            if (currentImage != null)
+            if (currentImage1 != null)
             {
                 //　画像を0,0に描画する
-                e.Graphics.DrawImage(currentImage,
-                    0, 0, currentImage.Width, currentImage.Height);
+                e.Graphics.DrawImage(currentImage1,
+                    0, 0, currentImage1.Width, currentImage1.Height);
 
             }
 
         }
-        
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentImage2 != null)
+            {
+                //　画像を0,0に描画する
+                e.Graphics.DrawImage(currentImage2,
+                    0, 0, currentImage2.Width, currentImage2.Height);
 
-            
-        
+            }
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentImage3 != null)
+            {
+                //　画像を0,0に描画する
+                e.Graphics.DrawImage(currentImage3,
+                    0, 0, currentImage3.Width, currentImage3.Height);
+
+            }
+
+        }
+
     }
+
 }
+
+
