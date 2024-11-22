1f01ab0b4bcebeaabe1552e48f4b2d7640d23541
MSI\kai08 <zeal24360040@fsgcl.jp>
2024-11-22 15:17:24 +0900

    ボタン、ラベルの配置

--- CS_Form2/CS_Form2/Form1.cs
+++ CS_Form2/CS_Form2/Form1.cs
@@ -12,10 +12,39 @@ namespace CS_Form2
 {
     public partial class Form1 : Form
     {
+      
 
+        //画像ファイルを読み込む
+        Image currentImage = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
         public Form1()
         {
             InitializeComponent();
+
+
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentImage != null)
+            {
+                //　画像を0,0に描画する
+                e.Graphics.DrawImage(currentImage,
+                    0, 0, currentImage.Width, currentImage.Height);
+
+            }
+
+        }
+        
+
+            
+        
     }
 }
