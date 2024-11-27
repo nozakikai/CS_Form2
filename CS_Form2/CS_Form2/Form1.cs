f44baaebc32139b142874846d34557c0de02ea52
MSI\kai08 <zeal24360040@fsgcl.jp>
2024-11-27 12:21:09 +0900

    勝敗判定関数

--- CS_Form2/CS_Form2/Form1.cs
+++ CS_Form2/CS_Form2/Form1.cs
@@ -26,10 +26,7 @@ namespace CS_Form2
         }
 
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("sakuma ゆいと");
-        }
+       
 
 
 
@@ -42,30 +39,108 @@ namespace CS_Form2
                     0, 0, currentImage1.Width, currentImage1.Height);
 
             }
-
         }
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
             if (currentImage2 != null)
             {
-                //　画像を0,0に描画する
                 e.Graphics.DrawImage(currentImage2,
                     0, 0, currentImage2.Width, currentImage2.Height);
 
             }
-
         }
-
         private void panel3_Paint(object sender, PaintEventArgs e)
         {
             if (currentImage3 != null)
             {
-                //　画像を0,0に描画する
+
                 e.Graphics.DrawImage(currentImage3,
                     0, 0, currentImage3.Width, currentImage3.Height);
 
             }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sakuma ゆいと");
+        }
 
+        private string GetCpuHand()
+        {
+        Random random = new Random();
+            int num = random.Next(3);
+            string hand = "";
+            if (num == 0)
+            {
+                hand = "グー";
+            }
+            else if (num == 1) 
+            {
+                hand = "チョキ";
+            }
+            else 
+            {
+                hand = "パー";
+            }
+            return hand;
+        }
+        private string Decides(string playerHand, string cpuHand)///playerの手とcpuの手を確認して勝敗を判定する
+        {
+            string result = "";
+
+            if (playerHand == "グー")          ///グーを出したとき
+            {
+                if (cpuHand == "グー")
+                {
+                    result = "DRAW";
+                }
+                else if (cpuHand == "チョキ")
+                {
+                    result = "WIN";
+                }
+                else
+                {
+                    result = "LOSE";
+                }
+            }
+            else if(playerHand == "チョキ")    ///チョキを出したとき　
+            {
+                if (cpuHand == "チョキ")
+                {
+                    result = "DRAW";
+                }
+                else if (cpuHand == "パー")
+                {
+                    result = "WIN";
+                }
+                else
+                {
+                    result = "LOSE";
+                }
+            }
+            else                                ///パーを出したとき
+            {
+
+                if (cpuHand == "パー")
+                {
+                    result = "DRAW";
+                }
+                else if (cpuHand == "グー")
+                {
+                    result = "WIN";
+                }
+                else
+                {
+                    result = "LOSE";
+                }
+            }
+            return result;
+        }
+
+        private void Progress(string playerHand)
+        {
+            string cpuHand = GetCpuHand();
+
+            string result = Decides(playerHand, cpuHand);
         }
 
     }
