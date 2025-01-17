1fd0fb54aced777d6b4007dce697278937f05d8e
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-01-17 15:24:23 +0900

    勝敗カウント、画像の表示・非表示切り替え　追加

--- CS_Form2/CS_Form2/Form1.Designer.cs
+++ CS_Form2/CS_Form2/Form1.Designer.cs
@@ -28,28 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.labelCPU = new System.Windows.Forms.Label();
             this.labelPLAYER = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGameCount = new System.Windows.Forms.Label();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PictureBox3 = new System.Windows.Forms.PictureBox();
+            this.PictureBox4 = new System.Windows.Forms.PictureBox();
+            this.PictureBox5 = new System.Windows.Forms.PictureBox();
+            this.PictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lblwin = new System.Windows.Forms.Label();
+            this.lbldraw = new System.Windows.Forms.Label();
+            this.lbllose = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox6)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(31, 255);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 250);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // labelCPU
             // 
             this.labelCPU.AutoSize = true;
-            this.labelCPU.Location = new System.Drawing.Point(468, 41);
+            this.labelCPU.Location = new System.Drawing.Point(411, 35);
             this.labelCPU.Name = "labelCPU";
             this.labelCPU.Size = new System.Drawing.Size(129, 18);
             this.labelCPU.TabIndex = 1;
@@ -58,88 +66,214 @@
             // labelPLAYER
             // 
             this.labelPLAYER.AutoSize = true;
-            this.labelPLAYER.Location = new System.Drawing.Point(468, 187);
+            this.labelPLAYER.Location = new System.Drawing.Point(411, 340);
             this.labelPLAYER.Name = "labelPLAYER";
             this.labelPLAYER.Size = new System.Drawing.Size(129, 18);
             this.labelPLAYER.TabIndex = 2;
             this.labelPLAYER.Text = "グー、チョキ、パー";
             // 
-            // button1
+            // Button1
             // 
-            this.button1.Location = new System.Drawing.Point(99, 556);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 36);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "グー";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Button1.Location = new System.Drawing.Point(180, 600);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(100, 30);
+            this.Button1.TabIndex = 3;
+            this.Button1.Text = "グー";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Button2
             // 
-            this.button2.Location = new System.Drawing.Point(480, 556);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 36);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "チョキ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Button2.Location = new System.Drawing.Point(430, 600);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(100, 30);
+            this.Button2.TabIndex = 4;
+            this.Button2.Text = "チョキ";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(872, 556);
+            this.button3.Location = new System.Drawing.Point(680, 600);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 36);
+            this.button3.Size = new System.Drawing.Size(100, 30);
             this.button3.TabIndex = 5;
             this.button3.Text = "パー";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
-            // panel2
+            // button4
             // 
-            this.panel2.Location = new System.Drawing.Point(404, 255);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(250, 250);
-            this.panel2.TabIndex = 0;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(920, 490);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 120);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "　終了";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // panel3
+            // label1
             // 
-            this.panel3.Location = new System.Drawing.Point(812, 255);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(250, 250);
-            this.panel3.TabIndex = 0;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(142, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 28);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CPUが出した手";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(100, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "あなたの出した手";
+            // 
+            // lblGameCount
+            // 
+            this.lblGameCount.AutoSize = true;
+            this.lblGameCount.Location = new System.Drawing.Point(917, 47);
+            this.lblGameCount.Name = "lblGameCount";
+            this.lblGameCount.Size = new System.Drawing.Size(80, 18);
+            this.lblGameCount.TabIndex = 9;
+            this.lblGameCount.Text = "試合回数";
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(120, 380);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(220, 200);
+            this.PictureBox1.TabIndex = 10;
+            this.PictureBox1.TabStop = false;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Location = new System.Drawing.Point(370, 380);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(220, 200);
+            this.PictureBox2.TabIndex = 10;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.Location = new System.Drawing.Point(620, 380);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(250, 200);
+            this.PictureBox3.TabIndex = 10;
+            this.PictureBox3.TabStop = false;
+            // 
+            // PictureBox4
+            // 
+            this.PictureBox4.Location = new System.Drawing.Point(120, 101);
+            this.PictureBox4.Name = "PictureBox4";
+            this.PictureBox4.Size = new System.Drawing.Size(220, 200);
+            this.PictureBox4.TabIndex = 10;
+            this.PictureBox4.TabStop = false;
+            // 
+            // PictureBox5
+            // 
+            this.PictureBox5.Location = new System.Drawing.Point(370, 101);
+            this.PictureBox5.Name = "PictureBox5";
+            this.PictureBox5.Size = new System.Drawing.Size(220, 200);
+            this.PictureBox5.TabIndex = 10;
+            this.PictureBox5.TabStop = false;
+            // 
+            // PictureBox6
+            // 
+            this.PictureBox6.Location = new System.Drawing.Point(620, 101);
+            this.PictureBox6.Name = "PictureBox6";
+            this.PictureBox6.Size = new System.Drawing.Size(250, 200);
+            this.PictureBox6.TabIndex = 10;
+            this.PictureBox6.TabStop = false;
+            // 
+            // lblwin
+            // 
+            this.lblwin.AutoSize = true;
+            this.lblwin.Location = new System.Drawing.Point(917, 112);
+            this.lblwin.Name = "lblwin";
+            this.lblwin.Size = new System.Drawing.Size(40, 18);
+            this.lblwin.TabIndex = 9;
+            this.lblwin.Text = "勝ち";
+            // 
+            // lbldraw
+            // 
+            this.lbldraw.AutoSize = true;
+            this.lbldraw.Location = new System.Drawing.Point(917, 152);
+            this.lbldraw.Name = "lbldraw";
+            this.lbldraw.Size = new System.Drawing.Size(73, 18);
+            this.lbldraw.TabIndex = 9;
+            this.lbldraw.Text = "引き分け";
+            // 
+            // lbllose
+            // 
+            this.lbllose.AutoSize = true;
+            this.lbllose.Location = new System.Drawing.Point(917, 192);
+            this.lbllose.Name = "lbllose";
+            this.lbllose.Size = new System.Drawing.Size(41, 18);
+            this.lbllose.TabIndex = 9;
+            this.lbllose.Text = "負け";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1387, 697);
+            this.ClientSize = new System.Drawing.Size(1178, 644);
+            this.Controls.Add(this.PictureBox6);
+            this.Controls.Add(this.PictureBox3);
+            this.Controls.Add(this.PictureBox5);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.PictureBox4);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.lbllose);
+            this.Controls.Add(this.lbldraw);
+            this.Controls.Add(this.lblwin);
+            this.Controls.Add(this.lblGameCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
             this.Controls.Add(this.labelPLAYER);
             this.Controls.Add(this.labelCPU);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label labelCPU;
         private System.Windows.Forms.Label labelPLAYER;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGameCount;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.PictureBox PictureBox2;
+        private System.Windows.Forms.PictureBox PictureBox3;
+        private System.Windows.Forms.PictureBox PictureBox4;
+        private System.Windows.Forms.PictureBox PictureBox5;
+        private System.Windows.Forms.PictureBox PictureBox6;
+        private System.Windows.Forms.Label lblwin;
+        private System.Windows.Forms.Label lbldraw;
+        private System.Windows.Forms.Label lbllose;
     }
 }
 
--- CS_Form2/CS_Form2/Form1.cs
+++ CS_Form2/CS_Form2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,57 +13,45 @@ namespace CS_Form2
 {
     public partial class Form1 : Form
     {
-
+        int gameCount = 0;
+        int winCount = 0;
+        int drawCount = 0;
+        int loseCount = 0;
 
         //画像ファイルを読み込む
-        Image currentImage1 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
-        Image currentImage2 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\チョキ.png");
-        Image currentImage3 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\パー.png");
+        // Image currentImage1 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
+        // Image currentImage2 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\チョキ.png");
+        // Image currentImage3 = Image.FromFile(@"C:\Users\kai08\Desktop\自由\画像\パー.png");
+
+
         public Form1()
         {
             InitializeComponent();
+            PictureBox1.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー.png");
+            PictureBox2.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\チョキ.png");
+            PictureBox3.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\パー.png");
 
+            PictureBox4.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\グー2.png");
+            PictureBox5.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\チョキ2.png");
+            PictureBox6.Image = Bitmap.FromFile(@"C:\Users\kai08\Desktop\自由\画像\パー2.png");
 
+            lblGameCount.Text = $"試合回数: {gameCount} 回";
         }
+        //12月6日　基本的構造完成
+        //次回以降　　勝敗カウント、カウントのリセットボタンを追加
+        //
+        //12月13日　cpuの画像の挿入
+        //終了ボタンの中央ぞろえ
+        //
+        
 
 
-       
 
 
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            if (currentImage1 != null)
-            {
-                //　画像を0,0に描画する
-                e.Graphics.DrawImage(currentImage1,
-                    0, 0, currentImage1.Width, currentImage1.Height);
-
-            }
-        }
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            if (currentImage2 != null)
-            {
-                e.Graphics.DrawImage(currentImage2,
-                    0, 0, currentImage2.Width, currentImage2.Height);
-
-            }
-        }
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-            if (currentImage3 != null)
-            {
-
-                e.Graphics.DrawImage(currentImage3,
-                    0, 0, currentImage3.Width, currentImage3.Height);
-
-            }
-        }
         private void button1_Click(object sender, EventArgs e)
         {
-            // MessageBox.Show("グー");
             Progress("グー");
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -75,21 +64,47 @@ namespace CS_Form2
             Progress("パー");
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 終了確認メッセージを表示
+            var result = MessageBox.Show("ゲームを終了しますか？", "終了確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // ユーザーが「はい」を選択した場合、ゲームを終了
+            if (result == DialogResult.Yes)
+            {
+                var result2 = MessageBox.Show("このゲームが終了してしまいます！本当によろしいですか？", "終了確認の終了確認", MessageBoxButtons.YesNo, MessageBoxIcon.Error);//Exclamation
+
+                if (result2 == DialogResult.Yes)
+                {
+                    Application.Exit(); // アプリケーション終了
+                }
+            }
+        }
+
         private string GetCpuHand()
         {
-        Random random = new Random();
+            Random random = new Random();
             int num = random.Next(3);
             string hand = "";
             if (num == 0)
             {
+                PictureBox5.Visible = false;
+                PictureBox6.Visible = false;
+
                 hand = "グー";
+                
             }
             else if (num == 1) 
             {
+                PictureBox4.Visible = false;
+                PictureBox6.Visible = false;
+
                 hand = "チョキ";
             }
             else 
             {
+                PictureBox4.Visible = false;
+                PictureBox5.Visible = false;
                 hand = "パー";
             }
             return hand;
@@ -98,55 +113,75 @@ namespace CS_Form2
         {
             string result = "";
 
+            
+
             if (playerHand == "グー")          ///グーを出したとき
             {
+                PictureBox2.Visible = false;
+                PictureBox3.Visible = false;
+
                 if (cpuHand == "グー")
                 {
-                    result = "DRAW　引き分けです";
+                    result = "DRAW　引き分けです。リトライだ！";
+                    drawCount++;
                 }
                 else if (cpuHand == "チョキ")
                 {
                     result = "WIN　あなたの勝利です。おめでとう！";
+                    winCount++;
                 }
                 else
                 {
                     result = "LOSE　あなたの負けです。悔しい！";
+                    loseCount++;
                 }
             }
             else if(playerHand == "チョキ")    ///チョキを出したとき　
             {
+                PictureBox1.Visible = false;
+                PictureBox3.Visible = false;
                 if (cpuHand == "チョキ")
                 {
-                    result = "DRAW　引き分けです";
+                    result = "DRAW　引き分けです。リトライだ！";
+                    drawCount++;
                 }
                 else if (cpuHand == "パー")
                 {
+
                     result = "WIN　あなたの勝利です。おめでとう！";
+                    winCount++;
                 }
                 else
                 {
                     result = "LOSE　あなたの負けです。悔しい！";
+                    loseCount++;
                 }
             }
             else                                ///パーを出したとき
             {
+                PictureBox1.Visible = false;
+                PictureBox2.Visible = false;
 
                 if (cpuHand == "パー")
                 {
-                    result = "DRAW　引き分けです";
+                    result = "DRAW　引き分けです。リトライだ！";
+                    drawCount++;
                 }
                 else if (cpuHand == "グー")
                 {
-                    result = "WIN　あなたの勝利です。おめでとう！　";
+                    result = "WIN　あなたの勝利です。おめでとう！";
+                    winCount++;
                 }
                 else
                 {
                     result = "LOSE　あなたの負けです。悔しい！";
+                    loseCount++;
                 }
             }
             return result;
         }
-
+   
+        
         private void Progress(string playerHand)
         {
             string cpuHand = GetCpuHand();
@@ -158,14 +193,24 @@ namespace CS_Form2
 
             MessageBox.Show(result);
 
+             PictureBox1.Visible = true;
+             PictureBox2.Visible = true;
+             PictureBox3.Visible = true;
+             PictureBox4.Visible = true;
+             PictureBox5.Visible = true;
+             PictureBox6.Visible = true;
+
+            gameCount++;
+            lblGameCount.Text = $"試合回数: {gameCount} 回";
+            lblwin.Text = $"勝ち: {winCount} 回";
+            lbldraw.Text = $"引き分け: {drawCount} 回";
+            lbllose.Text = $"負け: {loseCount} 回";
+
             labelPLAYER.Text = ("グー、チョキ、パー");
             labelCPU.Text = ("グー、チョキ、パー");
 
         }
-
-       
     }
-
 }
 
 
